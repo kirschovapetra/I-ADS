5ac8bfa1d8192a42d966eb080c173abb0c08a018
xkirschova <xkirschova@stuba.sk>
2021-03-21 13:32:37 +0100

    z1 finished

--- 02_Zadania/Zadanie01/classes.py
+++ 02_Zadania/Zadanie01/classes.py
@@ -1,5 +1,3 @@
-from termcolor import colored
-
 
 class Word:
     def __init__(self, frequency, value):
@@ -8,85 +6,19 @@ class Word:
 
 
 class Tree:
-    def __init__(self, value):
+    def __init__(self, value=""):
         self.value = value          # hodnota kluca
         self.left_subtree = None    # lavy podstrom
         self.right_subtree = None   # pravy podstrom
+
+    def set_value(self, value):
+        self.value = value
 
     def set_left_subtree(self, left_sub):
         self.left_subtree = left_sub
 
     def set_right_subtree(self, right_sub):
         self.right_subtree = right_sub
-
-    def pocet_porovnani(self, search_string, comparisons=1, print_results=True):
-        """
-        Funkcia vrati pocet porovnani, ktore sa vykonaju pocas hladania vstupneho retazca
-        v zostrojenom optimalnom binarnom vyhladavacom strome.
-        :param search_string: hladane slovo
-        :param comparisons: pocet porovnani
-        :param print_results: zobrazit vypisy? true/false
-        :return: pocet porovnani
-        """
-
-        LEFT = self.left_subtree    # lavy podstrom
-        RIGHT = self.right_subtree  # pravy podstrom
-        CURRENT = self.value        # aktualny uzol
-
-        if print_results:
-            self.print_comparison(comparisons, search_string)
-
-        # retazec je mensi => ide do laveho podstromu
-        if search_string < CURRENT:
-
-            # rekurzivne do laveho podstromu, o 1 viac porovnani
-            if LEFT is not None:
-                return LEFT.pocet_porovnani(search_string, comparisons + 1, print_results)
-
-            # koniec stromu => dostal sa na dummy kluc
-            if print_results:
-                self.print_comparison(comparisons, search_string, dummy=True)
-            return comparisons + 1
-
-        # retazec je vacsi => ide do praveho podstromu
-        elif search_string > CURRENT:
-
-            # rekurzivne do praveho podstromu, o 1 viac porovnani
-            if RIGHT is not None:
-                return RIGHT.pocet_porovnani(search_string, comparisons + 1, print_results)
-
-            # koniec stromu => dostal sa na dummy kluc
-            if print_results:
-                self.print_comparison(comparisons, search_string, dummy=True)
-            return comparisons + 1
-
-        # retazec bol najdeny medzi klucmi
-        return comparisons
-
-    def print_comparison(self, comparisons, search_string, dummy=False):
-        """
-        Vypis pre aktualne porovnanie retazcov
-        :param comparisons: poradove cislo porovnania
-        :param search_string: hladane slovo
-        :param dummy: dostal sa na dummy kluc? True/False
-        """
-
-        # dostane sa na dummy kluc
-        if dummy:
-            print("\nPorovnanie", str(comparisons + 1) + ":",
-                  colored("\n  -- DUMMY kluc: " + search_string + " --", "green", attrs=['bold']),
-                  "\n  Hladany retazec:", colored(search_string, "green", attrs=['bold']),
-                  "\n  Zhoda:", colored(True, "green", attrs=['bold']),
-                  "\n\n*******************")
-
-        # medzivysledok
-        else:
-            color = "green" if self.value == search_string else "red"
-            print("\nPorovnanie", str(comparisons) + ":",
-                  "\n  Aktualny kluc:", colored(self.value, color, attrs=['bold']),
-                  "\n  Hladany retazec:", colored(search_string, color, attrs=['bold']),
-                  "\n  Zhoda:", colored(self.value == search_string, color, attrs=['bold']),
-                  "\n\n*******************")
 
     def print_levels(self, save_to_file=False):
         """
--- 02_Zadania/Zadanie01/main.py
+++ 02_Zadania/Zadanie01/main.py
@@ -6,7 +6,6 @@ from termcolor import colored
 from tqdm import tqdm
 from classes import Tree, Word
 
-
 ''' ******************************************* POMOCNE FUNKCIE ***********************************************'''
 
 
@@ -21,34 +20,36 @@ def load_dictionary(filename):
     """
     Nacitanie slovnika zo suboru + lexikograficke usporiadanie
     :param filename: nazov suboru
-    :return: words (lexikograficky usporiadane), freq_sum (celkova frekvencia)
+    :return: word_list (lexikograficky usporiadane), freq_sum (celkova frekvencia)
     """
 
-    words = []
+    word_list = []
     freq_sum = 0
 
     # nacitanie zo suboru
     with open(filename) as f:
         for line in f:
             freq, val = line.split()
-            words.append(Word(int(freq), str(val)))
+            word_list.append(Word(int(freq), val))
             freq_sum += int(freq)
 
     # lexikograficke usporiadanie slov
-    return sorted(words, key=operator.attrgetter('value')), freq_sum
+    word_list_sorted = sorted(word_list, key=operator.attrgetter('value'))
+
+    return word_list_sorted, freq_sum
 
 
-def search_all(tree, words):
+def search_all(search_tree, word_list):
     """
     Prehladavanie vsetkych slov z dictionary.txt
-    :param tree: strom
-    :param words: pole slov = Word(frequency, value)
+    :param search_tree: vyhladavaci strom
+    :param word_list: pole slov Word(frequency, value)
     """
 
     to_write = ""
-    for w in words:
-        comparisons = tree.pocet_porovnani(search_string=w.value, print_results=False)
-        to_write += w.value + ":" + str(comparisons)+"\n"
+    for w in word_list:
+        comp = pocet_porovnani(search_tree, w.value, print_results=False)
+        to_write += w.value + ":" + str(comp) + "\n"
 
     with open("search.txt", "w") as f:
         f.write(to_write)
@@ -58,27 +59,27 @@ def search_all(tree, words):
 ''' ************************************************ BST ****************************************************'''
 
 
-def get_keys(words, freq_sum):
+def get_keys(word_list, freq_sum):
     """
     Nacitanie klucov a pravdepodobnosti zo slov
-    :param words: slova z dictionary.txt = Word(frequency, value)
+    :param word_list: pole slov Word(frequency, value)
     :param freq_sum: celkova frekvencia
     :return: key_words, key_probabilities, dummy_probabilities, KEYS_COUNT
     """
 
-    key_words = []              # k[]
-    key_probabilities = []      # p[]
-    dummy_probabilities = []    # q[]
+    key_words = []  # pole klucov (k)
+    key_probabilities = []  # pravdepodobnosti klucov (p)
+    dummy_probabilities = []  # pravdepodobnosti dummy (q)
 
-    frequency_between = 0       # q_0 = 0 / freq_sum
+    frequency_between = 0  # q_0 = 0 / freq_sum
 
-    for word in words:
+    for word in word_list:
         if word.frequency <= 50000:  # slova medzi klucmi k_i a k_i+1
             frequency_between += word.frequency
 
         else:  # novy kluc k_i
             key_words.append(word.value)  # k_i
-            key_probabilities.append(word.frequency / freq_sum)       # p_i
+            key_probabilities.append(word.frequency / freq_sum)  # p_i
             dummy_probabilities.append(frequency_between / freq_sum)  # q_i
             frequency_between = 0
     # q_n
@@ -88,7 +89,7 @@ def get_keys(words, freq_sum):
 
     # pd.Series s posunutym indexom 1..n
     key_words = pd.Series(key_words, index=range_inclusive(1, KEYS_COUNT))  # k[1..n]
-    key_probabilities = pd.Series(key_probabilities,  index=range_inclusive(1, KEYS_COUNT))  # p[1..n]
+    key_probabilities = pd.Series(key_probabilities, index=range_inclusive(1, KEYS_COUNT))  # p[1..n]
 
     dummy_probabilities = pd.Series(dummy_probabilities)  # q[0..n]
 
@@ -98,10 +99,11 @@ def get_keys(words, freq_sum):
 def optimal_BST(p, q, KEYS_COUNT):
     """
     Vytvorenie optimalneho BST
+    zdroj: https://edutechlearners.com/download/Introduction_to_algorithms-3rd%20Edition.pdf#page=423
     :param p: pravdepodobnosti klucov p[1..n]
     :param q: dummy pravdepodobnosti  q[0..n]
-    :param KEYS_COUNT: pocet klucov = 151
-    :return: COST_TABLE, ROOT_TABLE
+    :param KEYS_COUNT: pocet klucov (151)
+    :return: COST_TABLE (tabulka optimalnych cien), ROOT_TABLE (tabulka root indexov)
     """
 
     # COST_TABLE[1..n+1, 0..n]
@@ -112,13 +114,13 @@ def optimal_BST(p, q, KEYS_COUNT):
     PROBABILITY_SUM_TABLE = COST_TABLE.copy()
 
     # ROOT_TABLE[1..n, 1..n]
-    ROOT_TABLE = pd.DataFrame(np.zeros((KEYS_COUNT, KEYS_COUNT)),    # matica naplnena nulami
+    ROOT_TABLE = pd.DataFrame(np.zeros((KEYS_COUNT, KEYS_COUNT)),  # matica naplnena nulami
                               index=range_inclusive(1, KEYS_COUNT),  # [1..n] x [1..n]
                               columns=range_inclusive(1, KEYS_COUNT),
                               dtype=int)
 
-    for x in tqdm(iterable=range_inclusive(1, KEYS_COUNT)):  # riadky matice
-        for i in range_inclusive(1, KEYS_COUNT - x + 1):     # stlpce matice
+    for x in tqdm(iterable=range_inclusive(1, KEYS_COUNT)):  # riadky matice 1..n
+        for i in range_inclusive(1, KEYS_COUNT - x + 1):  # stlpce matice 1..n-x+1
             j = i + x - 1  # posunutie nad diagonalu matice
             COST_TABLE.at[i, j] = np.inf
 
@@ -128,9 +130,9 @@ def optimal_BST(p, q, KEYS_COUNT):
             # prechadzam vsetky kluce s id = i..j a hladam, ktory z nich moze byt root
             for root_tmp in range_inclusive(i, j):
 
-                cost_left = COST_TABLE.at[i, root_tmp - 1]              # cena laveho podstromu (bunka vlavo)
-                cost_right = COST_TABLE.at[root_tmp + 1, j]             # cena praveho podstromu (bunka dole)
-                probability_current = PROBABILITY_SUM_TABLE.at[i, j]    # suma pravdepodobnosti pre aktualny uzol
+                cost_left = COST_TABLE.at[i, root_tmp - 1]  # cena laveho podstromu (bunka vlavo)
+                cost_right = COST_TABLE.at[root_tmp + 1, j]  # cena praveho podstromu (bunka dole)
+                probability_current = PROBABILITY_SUM_TABLE.at[i, j]  # suma pravdepodobnosti pre aktualny uzol
 
                 # nova cena = cena laveho podstromu + praveho podstromu + pravdepodobnost aktualneho uzla
                 cost_tmp = cost_left + cost_right + probability_current
@@ -146,6 +148,7 @@ def optimal_BST(p, q, KEYS_COUNT):
 def build_tree(keys, ROOT_TABLE, i, j):
     """
     Vytvorenie stromu podla ROOT_TABLE
+    zdroj: https://github.com/CarterZhou/algorithms_practice/blob/master/dp/OptimalBST.java
     :param keys: kluce [1..n]
     :param ROOT_TABLE: tabulka root uzlov pre rozne podstromy
     :param i: index riadku ROOT_TABLE
@@ -155,41 +158,94 @@ def build_tree(keys, ROOT_TABLE, i, j):
 
     # index rootu aktualneho podstromu (so suradnicami [i, j] v ROOT_TABLE)
     root_id = ROOT_TABLE.at[i, j]
-
     # novy podstrom
     search_tree = Tree(keys.get(root_id))
 
-    # lavy podstrom = mensie indexy i (riadky)
+    # rekurzivne do laveho podstromu
     if i < root_id:
-        # rekurzivne do laveho podstromu
         search_tree.set_left_subtree(
             build_tree(keys, ROOT_TABLE, i, root_id - 1))  # root podstromu v bunke vlavo
 
-    # pravy podstrom = vacsie indexy j (stlpce)
+    # rekurzivne do praveho podstromu
     if j > root_id:
-        # rekurzivne do praveho podstromu
         search_tree.set_right_subtree(
             build_tree(keys, ROOT_TABLE, root_id + 1, j))  # root podstromu v bunke dole
 
     return search_tree
 
 
-''' ************************************************ MAIN ****************************************************'''
+def pocet_porovnani(node, search_string, comparisons=1, print_results=True):
+    """
+    Funkcia vrati pocet porovnani, ktore sa vykonaju pocas hladania vstupneho retazca
+    v zostrojenom optimalnom binarnom vyhladavacom strome.
+    zdroj: https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+    :param node: aktualny uzol stromu
+    :param search_string: hladane slovo
+    :param comparisons: pocet porovnani
+    :param print_results: zobrazit vypisy? true/false
+    :return: pocet porovnani
+    """
+    # vypis
+    if print_results:
+        print_comparison(node, comparisons, search_string)
 
+    # dostal sa na dummy kluc alebo bol najdeny medzi klucmi
+    if node is None or node.value == search_string:
+        return comparisons
+
+    LEFT = node.left_subtree  # lavy podstrom
+    RIGHT = node.right_subtree  # pravy podstrom
+    CURRENT = node.value  # aktualny uzol
+
+    # retazec je mensi - ide do laveho podstromu
+    if search_string < CURRENT:
+        return pocet_porovnani(LEFT, search_string, comparisons + 1, print_results)
+
+    # retazec je vacsi - ide do praveho podstromu
+    return pocet_porovnani(RIGHT, search_string, comparisons + 1, print_results)
+
+
+def print_comparison(node, comparisons, search_string):
+    """
+    Vypis pre aktualne porovnanie retazcov
+    :param node: aktualny uzol stromu
+    :param comparisons: poradove cislo porovnania
+    :param search_string: hladane slovo
+    """
+
+    # dostane sa na dummy kluc
+    if node is None:
+        print("\nPorovnanie", str(comparisons + 1) + ":",
+              colored("\n  -- DUMMY kluc: " + search_string + " --", "green", attrs=['bold']),
+              "\n  Hladany retazec:", colored(search_string, "green", attrs=['bold']),
+              "\n  Zhoda:", colored(True, "green", attrs=['bold']),
+              "\n\n*******************")
+
+    # medzivysledok
+    else:
+        color = "green" if node.value == search_string else "red"
+        print("\nPorovnanie", str(comparisons) + ":",
+              "\n  Aktualny kluc:", colored(node.value, color, attrs=['bold']),
+              "\n  Hladany retazec:", colored(search_string, color, attrs=['bold']),
+              "\n  Zhoda:", colored(node.value == search_string, color, attrs=['bold']),
+              "\n\n*******************")
+
+
+''' ************************************************ MAIN ****************************************************'''
 
 if __name__ == '__main__':
 
     # nacitanie a vytvorenie klucov a pravdepodobnosti
 
     words, freq_sum = load_dictionary("dictionary.txt")
-    key_words, key_probabilities, dummy_probabilities, KEYS_COUNT = get_keys(words, freq_sum)
+    keys, key_prob, dummy_prob, n = get_keys(words, freq_sum)
 
     # strom
 
     print(colored("\n***** Optimalny binarny vyhladavaci strom ******\n", color="cyan"))
-    cost_tab, root_tab = optimal_BST(key_probabilities, dummy_probabilities, KEYS_COUNT)
-    print("\nOptimalna cena: ", cost_tab.at[1, KEYS_COUNT])     # optimalna cena hladania
-    tree = build_tree(key_words, root_tab, 1, KEYS_COUNT)       # vytvorenie stromu
+    cost_tab, root_tab = optimal_BST(key_prob, dummy_prob, n)
+    print("\nOptimalna cena: ", cost_tab.at[1, n])  # optimalna cena hladania
+    tree = build_tree(keys, root_tab, 1, n)  # vytvorenie stromu
 
     sleep(0.5)
     print(colored("\n*************** Urovne stromu: ****************", color="cyan"))
@@ -198,13 +254,13 @@ if __name__ == '__main__':
 
     # pocet porovnani
 
-    print(colored("\n*************** Pocet porovnani ****************\n",color="cyan"))
+    print(colored("\n*************** Pocet porovnani ****************\n", color="cyan"))
 
     print("-- test --\n")
-    test_words = ["the", "i", "lasagna", "aaagh", "maths", "banana", "must"]
+    test_words = ["slaughter", "the", "i", "lasagna", "aaagh", "maths", "banana", "must"]
     for word in test_words:
-        comparisons = tree.pocet_porovnani(search_string=word, print_results=False)
-        print("   "+word+":", colored(comparisons, "green", attrs=["bold"]))
+        comparisons = pocet_porovnani(tree, word, print_results=False)
+        print("   " + word + ":", colored(comparisons, "green", attrs=["bold"]))
 
     print("\n-- vsetky slova z dictionary.txt --")
     search_all(tree, words)
