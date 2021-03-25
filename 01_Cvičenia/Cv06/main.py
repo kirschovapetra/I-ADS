d1d5a79217ac5dd47712fa7720101918742d9894
xkirschova <xkirschova@stuba.sk>
2021-03-25 22:27:45 +0100

    cv6 teraz uz ozaj

--- 01_Cvičenia/Cv06/main.py
+++ 01_Cvičenia/Cv06/main.py
@@ -1,34 +1,42 @@
 import numpy as np
-from tqdm import tqdm
 
 
 def max_win(tokens):
     n = len(tokens)
 
-    # priebezne maximalne vyhry [start, end]
-    MAX_WIN = np.zeros((n, n), dtype=int)
+    PLAYER_TAB = np.zeros((n, n), dtype=int)
+    KRUPIER_TAB = np.zeros((n, n), dtype=int)
 
-    for interval_size in range(n):           # prechadzaju sa rozne velke intervaly
+    for interval_size in range(n):  # prechadzaju sa rozne velke intervaly
+
         for end in range(interval_size, n):  # koniec intervalu
             start = end - interval_size      # zaciatok intervalu
 
-            # obaja beru zo zaciatku => zoberu 'start' a 'start+1' prvok
-            start_start = MAX_WIN[start + 2][end] if (start + 2 <= end) else 0
-            # kazdy z ineho konca => zoberu 'start' a 'end' prvok
-            start_end = MAX_WIN[start + 1][end - 1] if (start + 1 <= end - 1) else 0
-            # obaja beru z konca => zoberu 'end' a 'end-1' prvok
-            end_end = MAX_WIN[start][end - 2] if (start <= end - 2) else 0
+            # parny pocet tahov => porovnavam iba koncove hodnoty
+            if interval_size % 2 == 0:
+                pick_from_start = tokens[start]  # zo zaciatku
+                pick_from_end = tokens[end]  # z konca
 
-            # maximalna vyhra zo zaciatku vs. z konca
-            MAX_WIN[start][end] = max(tokens[start] + min(start_start, start_end),
-                                      tokens[end] + min(start_end, end_end))
+            # neparny pocet tahov => pozeram sa na to, aku vyhru moze dostat krupier
+            else:
+                pick_from_start = tokens[start] + KRUPIER_TAB[start + 1, end]  # zo zaciatku
+                pick_from_end = tokens[end] + KRUPIER_TAB[start, end - 1]  # z konca
 
-    return MAX_WIN[0][n - 1]
+            # maximalna vyhra
+            if pick_from_start > pick_from_end:
+                PLAYER_TAB[start, end] = tokens[start] + KRUPIER_TAB[start + 1, end]  # hrac zoberie zo zaciatku
+                KRUPIER_TAB[start, end] = PLAYER_TAB[start + 1, end]  # krupierovi ostane <start+1, end>
+            else:
+                PLAYER_TAB[start, end] = tokens[end] + KRUPIER_TAB[start, end - 1]  # hrac zoberie z konca
+                KRUPIER_TAB[start, end] = PLAYER_TAB[start, end - 1]  # krupierovi ostane <start, end-1>
+
+    return PLAYER_TAB[0, n-1]
 
 
 if __name__ == '__main__':
     with open("cvicenie6data.txt") as f:
         file_content = list(map(int, f.read()))
 
-    # print(solve(file_content))
+    print(max_win([2, 6, 9, 1, 2, 9, 2, 8]))
+    print(max_win([2, 6, 9, 1, 2, 16, 2, 8]))
     print(max_win(file_content))
